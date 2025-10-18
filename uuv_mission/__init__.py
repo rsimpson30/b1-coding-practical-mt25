69086ea6172013b1d702688530096ca5062d8012
Robert Simpson <rsimpson30@outlook.com>
2025-10-18 21:38:38 +0100

    Setup new venv and sorted out packages

---
+++ uuv_mission/__init__.py
@@ -0,0 +1,5 @@
+from .dynamic import *
+from .terrain import *
+
+__all__ = ['Submarine', 'Mission', 'Trajectory', 'ClosedLoop', 'generate_random_multisine_timeseries', \
+           'generate_reference_and_limits', 'plot_mission_and_terrain', 'write_mission_to_csv']
